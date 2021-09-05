d598c8759501cf42c0f4baf02f995563953561bd
长江 <zhaochangjiang@huoyunren.com>
2021-09-05 12:17:00 +0800

    升级逻辑

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/juetun/base-wrapper/lib/app/app_start"
 	_ "github.com/juetun/base-wrapper/lib/app/init" // 加载公共插件项
-	. "github.com/juetun/base-wrapper/lib/plugins"  // 组件目录
-	"github.com/juetun/dashboard-api-main/basic/myplugins"
+	"github.com/juetun/base-wrapper/lib/app/middlewares"
+	. "github.com/juetun/base-wrapper/lib/plugins"      // 组件目录
 	_ "github.com/juetun/dashboard-api-main/web/router" // 加载路由信息
 )
 
@@ -14,11 +14,14 @@ func main() {
 		PluginJwt, // 加载用户验证插件,必须放在Redis插件后
 		PluginOss,
 		PluginAppMap,
-		myplugins.PluginUser, // 用户登录,jwt等用户信息逻辑处理
+		func(arg *app_start.PluginsOperate) (err error) {
+			middlewares.MiddleWareComponent= append(middlewares.MiddleWareComponent, middlewares.CrossOriginResourceSharing())
+			return
+		},
 	).LoadPlugins() // 加载插件动作
 	// 启动GIN服务
 	app_start.NewWebApplication(
-		// middlewares.SignHttp(), // 添加签名中间件
+		middlewares.SignHttp(), // 添加签名中间件
 	).LoadRouter(). // 记载gin 路由配置
 		Run()
 }
