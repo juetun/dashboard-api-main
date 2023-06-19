1f53ed2b4acb2803fc55ddce7f5894c4f51b131b
zhaocj <zhaocj2009@126.com>
2023-06-19 11:18:07 +0800

    12

--- pkg/parameters/trend_type.go
+++ pkg/parameters/trend_type.go
@@ -3,10 +3,14 @@ package parameters
 import (
 	"github.com/juetun/base-wrapper/lib/app/app_start"
 	"github.com/juetun/library/common/app_param"
+	"github.com/juetun/library/common/recommend"
 )
 
 //动态信息注册
 func PluginAppendTrendType(arg *app_start.PluginsOperate) (err error) {
+	if err = recommend.ReplaceTrendTypes(nil, &recommend.ArgReplaceTrendType{Types: SliceTrendType}); err != nil {
+		return
+	}
 	app_param.AppendTrendsTypesAsModelItemOptions(SliceTrendType)
 	return
 }
