9fe7d496fc9cf13a4c3ee102517935b4296031ae
zhaocj <zhaocj2009@126.com>
2023-08-03 22:56:02 +0800

    12

--- pkg/parameters/cache_key.go
+++ pkg/parameters/cache_key.go
@@ -9,42 +9,42 @@ import (
 var (
 	CacheParamConfig = map[string]*redis_pkg.CacheProperty{
 		"CacheHelpDocRelateByKeyUpdating": { //店铺变跟资质缓存的缓存Key
-			Key:    "m:help:relate_k:%d",
+			Key:    "m:help:relate_k:%v",
 			Expire: 12 * time.Hour,
 		},
 		"CacheKeyAdminUserWithUserHId": { //管理员用户存储
-			Key:    "p:adu:%s",
+			Key:    "p:adu:%v",
 			Expire: 30 * 24 * time.Hour,
 		},
 		"CacheKeyHelp": {
-			Key:    "p:hp:%s",
+			Key:    "p:hp:%v",
 			Expire: 30 * 24 * time.Hour,
 		},
 		"CacheKeyHelpWithId": {
-			Key:    "p:hp_id:%s",
+			Key:    "p:hp_id:%v",
 			Expire: 12 * time.Hour,
 		},
 		"CacheKeyAllHelpRelate": {
-			Key:    "p:hp_all:%s",
+			Key:    "p:hp_all:%v",
 			Expire: 12 * time.Hour,
 		},
 		//接口按照appname缓存的key
 		"CacheKeyImportWithAppKey": {
-			Key:    "p:app_c:%s",
+			Key:    "p:app_c:%v",
 			Expire: 7 * 24 * time.Hour,
 		},
 		//用户所属的管理组
 		"CacheKeyUserGroupWithAppKey": {
-			Key:    "p:u_grp:%d",
+			Key:    "p:u_grp:%v",
 			Expire: 7 * 24 * time.Hour,
 		},
 		//用户组每个接口权限缓存
 		"CacheKeyUserGroupAppImportWithAppKey": {
-			Key:    "p:u_gai:%d_%s",
+			Key:    "p:u_gai:%v_%v",
 			Expire: 7 * 24 * time.Hour,
 		},
 		"CacheMenuImportWithAppKey": { //菜单有的接口权限
-			Key:    "p:menu_ipt:%d_%s",
+			Key:    "p:menu_ipt:%v_%v",
 			Expire: 7 * 24 * time.Hour,
 		},
 	}
